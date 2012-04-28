3c24aaac30c4158d10c33adcc2ad18df4c3f6c0f
Sean Brady <sean@konciergemd.com>
2012-04-28 07:57:18 -0400

    Revert "Build out of Java-friendly swagger classes… does not compile."
    This reverts commit 92a64567b3c84c294fdea901bc0d46595b8bb3c6.

--- samples/java-play2/app/controllers/PetApiController.java
+++ samples/java-play2/app/controllers/PetApiController.java
@@ -1,30 +1,25 @@
 package controllers;
 
 import com.wordnik.swagger.core.*;
-import models.Pet;
-import play.mvc.Controller;
+import play.libs.Json;
 import play.mvc.Result;
 
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+
+import static play.mvc.Results.ok;
 
 @Api(value = "/pet", description = "Operations about pets")
-public class PetApiController extends Controller {
+public class PetApiController {
 
     @Path("/{id}")
     @ApiOperation(value = "Find pet by ID", notes = "Returns a pet when ID < 10. " +
             "ID > 10 or nonintegers will simulate API error conditions", responseClass = "Pet", httpMethod = "GET")
-//    @ApiParamsImplicit(
-//            @ApiParamImplicit(name = "id", value = "ID of pet that needs to be fetched", required = true, dataType = "String", paramType = "path",
-//                    allowableValues = "range[0,10]"))
+    @ApiParamsImplicit(
+            @ApiParamImplicit(name = "id", value = "ID of pet that needs to be fetched", required = true, dataType = "String", paramType = "path",
+                    allowableValues = "range[0,10]"))
     @ApiErrors({@ApiError(code = 400, reason = "Invalid ID supplied"),
                 @ApiError(code = 404, reason = "Pet not found")})
-
-    @QueryParam("status")
-    public static Result getPetById(@ApiParam(value = "Status values that need to be considered for filter", required = true, defaultValue = "available",
-            allowableValues = "available,pending,sold", allowMultiple = true) String id) {
-        Pet pet = new Pet();
-//        return JsonResponse(pet);
-        return ok();
+    public static Result getPetById(Long id) {
+        return ok(Json.toJson("hello pets!"));
     }
 }
--- samples/java-play2/app/models/Pet.java
+++ samples/java-play2/app/models/Pet.java
@@ -1,19 +1,13 @@
 package models;
 
-import play.db.ebean.Model;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class Pet extends Model {
+public class Pet {
     private Long id = 0L;
     //private Category category;
     private String name;
     private List<String> photoUrls = new ArrayList<String>();
     //private List<Tag> tags = new ArrayList<Tag>();
     private String status;
-
-    public static Pet findById(Long id) {
-        return new Pet();
-    }
 }
